2a78a0440c187b6fa293f3cce868bb1f3cd2ea7f
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-06-18 20:03:09 +0400

    feat: setup mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import connectDB from "./config/db";
 
 const app = express();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
