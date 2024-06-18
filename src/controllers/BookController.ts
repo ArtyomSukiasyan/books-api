18e405c778297f8fd93d53a26472acac266419a8
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-06-18 21:22:52 +0400

    feat: add bookService

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -1,70 +1,22 @@
 import { Request, Response } from "express";
-import Book from "../models/Book";
+import bookService from "../services/book.service";
 
 export async function createBook(req: Request, res: Response) {
-  const { title, author, publicationDate, genres } = req.body;
-  try {
-    const newBook = new Book({ title, author, publicationDate, genres });
-    const book = await newBook.save();
-
-    return res.json(book);
-  } catch (err) {
-    return res.status(500).send("Server Error");
-  }
+  return bookService.createBook(req, res);
 }
 
 export async function getBooks(req: Request, res: Response) {
-  try {
-    const books = await Book.find();
-
-    return res.json(books);
-  } catch (err) {
-    return res.status(500).send("Server Error");
-  }
+  return bookService.getBooks(req, res);
 }
 
 export async function getBookById(req: Request, res: Response) {
-  try {
-    const book = await Book.findById(req.params.id);
-    if (!book) {
-      return res.status(404).json({ msg: "Book not found" });
-    }
-
-    return res.json(book);
-  } catch (err) {
-    return res.status(500).send("Server Error");
-  }
+  return bookService.getBookById(req, res);
 }
 
 export async function updateBookById(req: Request, res: Response) {
-  const { title, author, publicationDate, genres } = req.body;
-  try {
-    let book = await Book.findById(req.params.id);
-    if (!book) {
-      return res.status(404).json({ msg: "Book not found" });
-    }
-    book.title = title;
-    book.author = author;
-    book.publicationDate = publicationDate;
-    book.genres = genres;
-    book = await book.save();
-
-    return res.json(book);
-  } catch (err) {
-    return res.status(500).send("Server Error");
-  }
+  return bookService.updateBookById(req, res);
 }
 
 export async function deleteBookById(req: Request, res: Response) {
-  try {
-    const book = await Book.findById(req.params.id);
-    if (!book) {
-      return res.status(404).json({ msg: "Book not found" });
-    }
-    await book.deleteOne({ _id: book._id });
-
-    return res.json({ msg: "Book removed" });
-  } catch (err) {
-    return res.status(500).send("Server Error");
-  }
+  return bookService.deleteBookById(req, res);
 }
